a2a32ab058141ff2bc6ea0dfdd289ee9589b583e
jesc7 <jesc@mail.ru>
2026-01-06 23:15:32 +0400

    --- Auto Git Commit ---

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -1,9 +1,17 @@
 <template>
-	<div>hi there! lalala</div>
+	
+	<div id="offer">hi there! lalala</div>
+	<div id="answer">hi there! lalala</div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+	#offer {
+		background-color: green;
+	}
+	#answer {
+		background-color: chocolate;
+	}
 </style>
