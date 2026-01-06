97048780940e5013d1d8e309ec9967d4604b88e6
jesc7 <jesc@mail.ru>
2026-01-07 00:01:30 +0400

    --- Auto Git Commit ---

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -1,13 +1,8 @@
 <template>
-<div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  <img class="size-12 shrink-0" alt="ChitChat Logo" />
-  <div>
-    <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
-    <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
-  </div>
-</div>	
-	<div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10" id="offer">hi there! lalala</div>
-	<div id="answer">hi there! lalala</div>
+	<div class="flex flex-col items-center gap-6 p-7 rounded-2xl border-2">
+		<div>123</div>
+		<div>456</div>
+	</div>
 </template>
 
 <script setup>
