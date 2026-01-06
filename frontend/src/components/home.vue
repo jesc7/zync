6a20e0600ce2264e52f8f9b32c0e3e47062c40dd
jesc7 <jesc@mail.ru>
2026-01-07 00:42:22 +0400

    --- Auto Git Commit ---

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="h-screen w-screen flex items-center gap-4 p-4">
-		<div class="w-full h-full flex flex-col justify-center items-center bg-blue-400 rounded-2xl border">
-			<div class="h-1/2">1</div>
-			<div>2</div>
-			<div>3</div>
+		<div class="w-full h-full flex flex-col justify-center items-center">
+			<div class="h-6">
+				<label>
+					Key: 
+  					<input type="text" placeholder="type key here..." />
+  				</label>
+			</div>
 		</div>
-		<div class="w-full h-full rounded-md border">456</div>
+		<div class="w-0 h-full border-l"></div>
+		<div class="w-full h-full flex flex-col justify-center items-center">
+			<div class="h-6">2</div>
+		</div>
 	</div>
 </template>
 
