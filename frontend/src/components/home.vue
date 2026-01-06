51d9373b21df3ad4c4d4265951e063ddf7a7b6f0
jesc7 <jesc@mail.ru>
2026-01-07 00:32:09 +0400

    --- Auto Git Commit ---

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="h-screen w-screen flex items-center gap-4 p-4">
 		<div class="w-full h-full flex flex-col justify-center items-center bg-blue-400 rounded-2xl border">
-			<div>1</div>
+			<div class="h-1/2">1</div>
 			<div>2</div>
 			<div>3</div>
 		</div>
-		<div class="w-full h-full bg-emerald-500 rounded-2xl border">456</div>
+		<div class="w-full h-full rounded-md border">456</div>
 	</div>
 </template>
 
