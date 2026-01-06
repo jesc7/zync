244acaaaa2ac9277ecaaccef648e055e82e8cc13
jesc7 <jesc@mail.ru>
2026-01-07 00:52:34 +0400

    --- Auto Git Commit ---

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -2,11 +2,9 @@
 	<div class="h-screen w-screen flex items-center gap-4 p-4">
 		<div class="w-full h-full flex flex-col justify-center items-center">
 			<div class="h-6">
-				<label>
-					Key: 
-  					<input type="text" placeholder="type key here..." />
-  				</label>
-			</div>
+				<label for="key">Key:</label>
+  				<input type="text" id="key" placeholder="type key here..." />
+  			</div>
 		</div>
 		<div class="w-0 h-full border-l"></div>
 		<div class="w-full h-full flex flex-col justify-center items-center">
