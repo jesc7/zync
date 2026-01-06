d4d0d78e3b8aea47368cb05e6343cb1da7b1629f
jesc7 <jesc@mail.ru>
2026-01-06 19:56:00 +0400

    --- Auto Git Commit ---

--- go.mod
+++ go.mod
@@ -6,15 +6,27 @@ require (
 	github.com/gorilla/websocket v1.5.3
 	github.com/pion/mediadevices v0.9.0
 	github.com/pion/webrtc/v4 v4.2.1
+	github.com/wailsapp/wails/v2 v2.11.0
 )
 
 require (
+	github.com/bep/debounce v1.2.1 // indirect
 	github.com/gen2brain/shm v0.1.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/kbinani/screenshot v0.0.0-20250624051815-089614a94018 // indirect
+	github.com/labstack/echo/v4 v4.13.3 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/leaanthony/go-ansi-parser v1.6.1 // indirect
+	github.com/leaanthony/gosod v1.0.4 // indirect
+	github.com/leaanthony/slicer v1.6.0 // indirect
+	github.com/leaanthony/u v1.1.1 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pion/datachannel v1.5.10 // indirect
 	github.com/pion/dtls/v3 v3.0.9 // indirect
 	github.com/pion/ice/v4 v4.1.0 // indirect
@@ -30,9 +42,19 @@ require (
 	github.com/pion/stun/v3 v3.0.2 // indirect
 	github.com/pion/transport/v3 v3.1.1 // indirect
 	github.com/pion/turn/v4 v4.1.3 // indirect
+	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/samber/lo v1.49.1 // indirect
+	github.com/tkrajina/go-reflector v0.5.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/wailsapp/go-webview2 v1.0.22 // indirect
+	github.com/wailsapp/mimetype v1.4.1 // indirect
 	github.com/wlynxg/anet v0.0.5 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/image v0.23.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,152 +1,36 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"log"
-	"net/url"
-	"strings"
+	"embed"
 
-	"github.com/gorilla/websocket"
-	"github.com/pion/mediadevices"
-	"github.com/pion/mediadevices/pkg/codec/openh264"
-	_ "github.com/pion/mediadevices/pkg/driver/screen"
-	"github.com/pion/webrtc/v4"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-func Encode(v any) (string, error) {
-	b, e := json.Marshal(v)
-	if e != nil {
-		return "", e
-	}
-	return base64.StdEncoding.EncodeToString(b), nil
-}
-
-func Decode(in string, v any) error {
-	b, e := base64.StdEncoding.DecodeString(in)
-	if e != nil {
-		return e
-	}
-	return json.Unmarshal(b, v)
-}
-
-type Msg struct {
-	Type  int    `json:"type"`
-	Code  int    `json:"code"`
-	Error string `json:"error,omitzero"`
-	Key   string `json:"key,omitzero"`
-	Value string `json:"val,omitzero"`
-}
-
-func createOffer() (pc *webrtc.PeerConnection, offer webrtc.SessionDescription, e error) {
-	config := webrtc.Configuration{
-		ICEServers: []webrtc.ICEServer{
-			{URLs: []string{"stun:stun.l.google.com:19302"}},
-		},
-	}
-
-	// Create a new RTCPeerConnection
-	xParams, _ := openh264.NewParams()
-	xParams.UsageType = openh264.ScreenContentRealTime
-	xParams.BitRate = 1_000_000 // 500kbps
-
-	codecSelector := mediadevices.NewCodecSelector(
-		mediadevices.WithVideoEncoders(&xParams),
-	)
-
-	mediaEngine := webrtc.MediaEngine{}
-	codecSelector.Populate(&mediaEngine)
-	api := webrtc.NewAPI(webrtc.WithMediaEngine(&mediaEngine))
-	if pc, e = api.NewPeerConnection(config); e != nil {
-		return
-	}
-
-	s, e := mediadevices.GetDisplayMedia(mediadevices.MediaStreamConstraints{
-		Video: func(c *mediadevices.MediaTrackConstraints) {
-		},
-		Codec: codecSelector,
-	})
-	if e != nil {
-		return
-	}
-
-	for _, track := range s.GetTracks() {
-		if _, e = pc.AddTransceiverFromTrack(track,
-			webrtc.RTPTransceiverInit{
-				Direction: webrtc.RTPTransceiverDirectionSendonly,
-			},
-		); e != nil {
-			return
-		}
-	}
-
-	if offer, e = pc.CreateOffer(nil); e != nil {
-		return
-	}
-
-	gatherComplete := webrtc.GatheringCompletePromise(pc)
-	if e = pc.SetLocalDescription(offer); e != nil {
-		return
-	}
-	<-gatherComplete
-	return
-}
+//go:embed all:frontend/dist
+var assets embed.FS
 
 func main() {
-	//signaling server websocket conn
-	u := url.URL{Scheme: "ws", Host: "localhost:1212", Path: "/ws"}
-	conn, _, e := websocket.DefaultDialer.Dial(u.String(), nil)
-	if e != nil {
-		log.Fatal("dial:", e)
-	}
-	defer conn.Close()
+	// Create an instance of the app structure
+	app := NewApp()
 
-	pc, offer, e := createOffer()
-	if e != nil {
-		panic(e)
-	}
-	payload, e := Encode(offer)
-	if e != nil {
-		panic(e)
-	}
-	conn.WriteJSON(Msg{
-		Type:  0,
-		Value: payload,
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "myproject",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
 	})
 
-	var (
-		key, pwd string
-	)
-	for {
-		var msg Msg
-		if e := conn.ReadJSON(&msg); e != nil {
-			log.Printf("Read message error: %v", e)
-			break
-		}
-		if msg.Code < 0 {
-			panic(errors.New(msg.Error))
-		}
-		switch msg.Type {
-		case 0:
-			sl := strings.Split(msg.Key, "@")
-			if msg.Type != 0 || len(sl) < 2 {
-				panic(errors.New("Wrong key"))
-			}
-			key, pwd = sl[0], sl[1]
-			fmt.Printf("Key=%s, pwd=%s\n", key, pwd)
-
-		case 3:
-			var answer webrtc.SessionDescription
-			if e = Decode(msg.Value, answer); e != nil {
-				panic(e)
-			}
-			if e = pc.SetRemoteDescription(answer); e != nil {
-				panic(e)
-			}
-			// Block forever
-			select {}
-		}
+	if err != nil {
+		println("Error:", err.Error())
 	}
 }
