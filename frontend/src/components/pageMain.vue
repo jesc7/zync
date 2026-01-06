300b948ca271f7b6a59059559b91a22118f7d82f
jesc7 <jesc@mail.ru>
2026-01-06 21:48:27 +0400

    --- Auto Git Commit ---

--- frontend/src/components/pageMain.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>hi there!</div>
-</template>
-
-<script setup>
-import { ref } from 'vue'
-
-const app = window.go.backend.App
-</script>
--- frontend/src/router/routes.js
+++
@@ -1,9 +0,0 @@
-
-const routes = [
-  {
-    path: '/',
-    component: () => import('components/pageMain.vue'),
-  },
-]
-
-export default routes
