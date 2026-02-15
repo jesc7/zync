d36f64d72ec7e307f1e7a52065ff69d67b4a9f29
jesc7 <jesc@mail.ru>
2026-02-15 22:24:35 +0400

    --- Auto Git Commit ---

--- backend/app.go
+++ backend/app.go
@@ -2,17 +2,23 @@ package backend
 
 import (
 	"context"
+	"time"
 )
 
 type Data struct {
 	Offer    string
 	Key      string
 	Password string
+	Time     time.Time
 }
 
 type App struct {
 	ctx  context.Context
 	data Data
+}
+
+func (a *App) SetTime(value time.Time) {
+	a.data.Time = value
 }
 
 func NewApp() *App {
@@ -23,6 +29,13 @@ func NewApp() *App {
 
 func (a *App) OnStartup(ctx context.Context) {
 	a.ctx = ctx
+	go func() {
+		t := time.NewTicker(time.Second)
+		defer t.Stop()
+		for v := range t.C {
+			a.SetTime(v)
+		}
+	}()
 }
 
 func (a *App) OnBeforeClose(ctx context.Context) (prevent bool) {
