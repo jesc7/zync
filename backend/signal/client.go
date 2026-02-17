6aa9b17784d7e57a9aee33f3cf5dacaa2a8ee1ea
jesc7 <jesc@mail.ru>
2026-02-17 12:58:53 +0400

    --- Auto Git Commit ---

--- backend/signal/client.go
+++ backend/signal/client.go
@@ -1,10 +1,23 @@
 package signal
 
 import (
+	"errors"
 	"net/url"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	MT_NOANSWER      MessageType = iota - 1 //не отправлять ответ
+	MT_SENDOFFER                            //клиент1 отправил offer
+	MT_SENDANSWER                           //клиент2 отправил answer
+	MT_RECEIVEANSWER                        //клиенту1 отправили answer клиента2
+	MT_CONNECT                              //клиент1 уведомляет об установлении соединения
+	MT_DISCONNECT                           //клиент1 уведомляет о разрыве соединения
+	MT_PING                                 //ping
+	MT_PONG                                 //pong
 )
 
 type Msg struct {
@@ -43,8 +56,21 @@ func (c *Client) Close() {
 	c.conn.Close()
 }
 
-func SendOffer() {
+func (c *Client) SendOffer(offer string) (key, password string, e error) {
+	if e = c.conn.WriteJSON(Msg{
+		Type:  MT_SENDOFFER,
+		Value: offer,
+	}); e != nil {
+		return
+	}
 
+	var answer Msg
+	select {
+	case <-time.After(10 * time.Second):
+		e = errors.New("Timeout has expired")
+		return
+	}
+	c.conn.ReadJSON(&answer)
 }
 
 /*
